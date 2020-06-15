dfa8e06f9664698ac065c0e4877c78607a2603e3
root <root@21796db289a1>
2020-06-15 23:52:34 +0000

    I forget which day, much progress, its 6/15/20

--- src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
+++ src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
@@ -9,15 +9,15 @@ public class MadlibsOfTheAmazon {
 		String prompt =  "If you find yourself having to cross a piranha-infested river, here's how to do it...";
 		
 		// Get the player to enter an adjective
-
+		String adjective = JOptionPane.showInputDialog("Enter an adjective");
 		// Get the player to enter a type of liquid
-
+		String liquid = JOptionPane.showInputDialog("Enter a liquid");
 		// Get the player to enter a body part
-
+		String bodyPart = JOptionPane.showInputDialog("Ener a body part");
 		// Get the player to enter a verb
-
+		String verb = JOptionPane.showInputDialog("Enter a verb");
 		// Get the player to enter a place
-
+		String place = JOptionPane.showInputDialog("Enter a place");
 		// The story below has has been written as a group of Strings joined together by + signs.
 		// The story contains place holders, indicated by [** **] which you need to replace with
 		// the values entered by the player.
@@ -34,6 +34,11 @@ public class MadlibsOfTheAmazon {
 		// If you need to, move them around to make your story look better in the pop-up
 		
 		// If you want to write your own Madlib story, just change the story variable and ask the player different questions.
-
+		JOptionPane.showMessageDialog(null, "Piranhas are more " + adjective + " during the day, so cross the river at\n" +
+		"night. Piranhas are attracted to fresh " + liquid + " and will most\n" + 
+		"likely take a bite out of your " + bodyPart + " if you " + verb + ". Whatever\n" +
+		"you do, if you have an open wound, try to find another way to get\n"  +
+		"back to the " + place + ". Good luck!");
+				
 	}
 }
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,14 +1,19 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score = 0; 
 		// ASK A QUESTION AND CHECK THE ANSWER
-		
+		String question = JOptionPane.showInputDialog("Is League the most popular game in the world");
 				// 2.  Ask the user a question 
-				
+				if (question.equalsIgnoreCase("Yes")) {
+					
+					score++;
+				}
 				// 3.  Use an if statement to check if their answer is correct
 				
 				// 4.  if the user's answer was correct, add one to their score 
@@ -18,6 +23,6 @@ public class QuizGame {
 		
 		
 		// After all the questions have been asked, tell the user their final score 
-		
+		JOptionPane.showMessageDialog(null, "Your score is " + score);
 	}
 }
